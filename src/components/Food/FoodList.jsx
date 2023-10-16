3fe9312b4b64a93d6a0c55373cd9bdf92f0771cb
Dheeresh Agarwal <da11@iitbbs.ac.in>
2023-10-16 23:13:37 +0530

    icon fix

--- src/components/Food/FoodList.jsx
+++ src/components/Food/FoodList.jsx
@@ -1,10 +1,11 @@
+import { icons } from "react-icons";
 import FoodCard from "./FoodCard";
 import FoodListHeader from "./FoodListHeader";
 
-const FoodList = ({ foodData }) => {
+const FoodList = ({ foodData, foodName, icon }) => {
   return (
     <div>
-      <FoodListHeader foodName="Favorites" icon={"reddit"} />
+      <FoodListHeader foodName={foodName} icon={icon} />
       <div className="flex overflow-x-auto max-lg:no-scrollbar py-4">
         {foodData.map((food) => (
           <FoodCard key={food.image} food={food} />
